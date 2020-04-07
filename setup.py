0558329a1074671111461d06490b59b3dbb7ec9a
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-04-07 13:28:27 +0900

    version 1.0.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL="https://github.com/KoichiYasuoka/deplacy"
 
 setuptools.setup(
   name="deplacy",
-  version="1.0.6",
+  version="1.0.7",
   description="Simple dependency visualizer",
   long_description=long_description,
   long_description_content_type="text/markdown",
