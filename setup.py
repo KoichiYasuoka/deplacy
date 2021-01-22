fea1beb28b8847396fb4b80803ef77bdcb6a6654
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-01-22 18:02:14 +0900

    version 1.8.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL="https://github.com/KoichiYasuoka/deplacy"
 
 setuptools.setup(
   name="deplacy",
-  version="1.8.8",
+  version="1.8.9",
   description="CUI-based Tree Visualizer for Universal Dependencies and Immediate Catena Analysis",
   long_description=long_description,
   long_description_content_type="text/markdown",
